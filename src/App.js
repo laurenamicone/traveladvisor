d5002065c74218488c6c299d68bc68484f602e11
laurenamicone <laurenamicone@outlook.com>
2022-01-21 09:15:30 -0500

    initial checkin

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, {useEffect, useState} from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+
+import {getPlacesData, getWeatherData} from './api';
+
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+
+const App = () => {
+    const [places, setPlaces] = useState([]);
+    const [filteredPlaces, setFilteredPlaces] = useState([]);
+    const [coordinates, setCoordinates] = useState({});
+    const [bounds, setBounds] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [childClicked, setChildClicked] = useState(null);
+    const [type, setType] = useState('restaurants');
+    const [rating, setRating] = useState('0');
+    const [weatherData, setWeatherData] = useState([]);
+    const [autocomplete, setAutocomplete] = useState(null);
+   
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}}) => {
+            setCoordinates({lat: latitude, lng: longitude});
+        });
+    }, []);
+
+    useEffect(() => {
+        const filtered = places.filter((place) => place.rating > rating);
+        setFilteredPlaces(filtered);
+    }, [rating]);
+
+    useEffect(()=>{
+        if (bounds.sw && bounds.ne) {
+            setIsLoading(true);
+            getWeatherData(coordinates.lat, coordinates.lng)
+                .then((data) => setWeatherData(data));
+
+            getPlacesData(type, bounds.sw, bounds.ne)
+                .then((data) => {
+                    setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                    setFilteredPlaces([]);
+                    setIsLoading(false);
+                });
+        }
+    }, [type, bounds]);
+
+    const onLoad = (autoC) => setAutocomplete(autoC);
+    const onPlaceChanged = () => {
+        const lat = autocomplete.getPlace().geometry.location.lat();
+        const lng = autocomplete.getPlace().geometry.location.lng();
+
+        setCoordinates({lat, lng});
+    }
+
+    return (
+        <>
+            <CssBaseline />
+            <Header onLoad={onLoad} onPlaceChanged={onPlaceChanged} />
+            <Grid container spacing={3} style={{width: ' 100%' }}> 
+                <Grid item xs={12} md={4}>
+                    <List 
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        childClicked={childClicked}
+                        isLoading={isLoading}
+                        type={type}
+                        setType={setType}
+                        rating={rating}
+                        setRating={setRating}
+                    />
+                </Grid>
+                <Grid item xs={12} md={8}>
+                    <Map 
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked={setChildClicked}
+                        weatherData={weatherData}
+                    />
+                </Grid>
+            </Grid>
+        </>
+    );
+}
+
+export default App;
